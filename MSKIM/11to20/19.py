433fc22a967549509cac05d7d655097eb379543e
greenlemonT <ms950219@naver.com>
2024-05-15 14:44:02 +0900

    19

---
+++ MSKIM/11to20/19.py
@@ -0,0 +1,19 @@
+def polynomial_hash(str):
+    p=31
+    m=1_000_000_007
+    hash_value=0
+    for char in str:
+        hash_value=(hash_value*p+ord(char))%m
+    return hash_value
+
+def solution(string_list,query_list):
+    hash_list=[polynomial_hash(str) for str in string_list]
+
+    result=[]
+    for query_list in query_list:
+        query_hash=polynomial_hash(query)
+        if query_hash in hash_list:
+            result.append(True)
+        else:
+            result.append(False)
+    return result

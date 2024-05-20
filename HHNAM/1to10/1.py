5e6f16eeda05730b34a84354c9d99102a0556dae
Hypeho <n85943164@gmail.com>
2024-05-21 03:10:39 +0900

    1

--- HHNAM/1to10/1.py
+++ HHNAM/1to10/1.py
@@ -2,6 +2,7 @@ def solution(arr):
     arr.sort()
     return arr
 
+
 # print(solution([1,-5,2,4,3]))     # [-5, 1, 2, 3, 4]
 # print(solution([2,1,1,3,2,5,4]))  # [1, 1, 2, 2, 3, 4, 5]
 # print(solution([1,6,7]))          # [1, 6, 7]

66b74df6dfb2b2869cf7ef0da450b66fe13c6951
greenlemonT <ms950219@naver.com>
2024-07-02 13:20:42 +0900

    27

---
+++ MSKIM/21to30/27.py
@@ -0,0 +1,49 @@
+
+class Node:
+    def __init__(self,key):
+        self.left=None
+        self.right=None
+        self.val=key
+class BST:
+    def __init__(self):
+        self.root=None
+
+    def insert(self,key):
+        if not self.root:
+            self.root=Node(key)
+        else:
+            curr=self.root
+            while True:
+                if key<curr.val:
+                    if curr.left:
+                        curr=curr.left
+                    else:
+                        curr.left=Node(key)
+                        break
+                else:
+                    if curr.right:
+                        curr=curr.right
+                    else:
+                        curr.right=Node(key)
+                        break
+    def search(self,key):
+        curr=self.root
+        while curr and curr.val!=key:
+            if key<curr.val:
+                curr=curr.left
+            else:
+                curr=curr.right
+            return curr
+
+def solution(lst,search_lst):
+    bst=BST()
+    for key in lst:
+        bst.insert(key)
+    result=[]
+    for val in search_lst:
+        if bst.search(val):
+            result.append(True)
+        else:
+            result.append(False)
+    return result
+

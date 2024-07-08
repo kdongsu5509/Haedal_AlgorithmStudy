290729515bcc256c7b5c025e68422f2005eabd47
JeongMinR <ryu040408@naver.com>
2024-07-08 19:39:06 +0900

    05

--- JMRYU/1to10/05.py
+++ JMRYU/1to10/05.py
@@ -1,6 +1,5 @@
 def solution(arr1, arr2):
 
-    result = [[sum(a * b for a, b in zip(row, col)) 
-               for col in arr2_transposed = list(zip(*arr2))] for row in arr1]
+    answer = [[sum(arr1[i][k] * arr2[k][j] for k in range(len(arr1[0]))) for j in range(len(arr2[0]))] for i in range(len(arr1))]
 
-    return result
+    return answer

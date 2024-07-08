973683271cb43372a7234bea9d86fdf4fa0bf1b3
JeongMinR <ryu040408@naver.com>
2024-07-08 20:06:12 +0900

    05

--- JMRYU/1to10/05.py
+++ JMRYU/1to10/05.py
@@ -2,4 +2,4 @@ def solution(arr1, arr2):
 
     answer = [[sum(arr1[i][k] * arr2[k][j] for k in range(len(arr1[0]))) for j in range(len(arr2[0]))] for i in range(len(arr1))]
 
-    return answer 
+    return answer  

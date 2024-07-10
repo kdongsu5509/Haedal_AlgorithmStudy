6f52f0cf768bab2af96c7088703d3ea05df3f3dd
kdongsu5509 <kdongsu5509@gmail.com>
2024-07-10 20:54:31 +0900

    UPDATE : 27.py AND twoSeven.java

--- DSKO/21to30/28.py
+++ DSKO/21to30/28.py
@@ -1,10 +1,21 @@
-import math
+def newNum(a):
+    if a % 2 == 0:
+        return a // 2
+    else:
+        return (a+1) // 2
 
 def solution(n,a,b):
+    #총 인원 수, a=참가자1의 번호 , b = 참가자 2의 번호
     cnt = 0
-    while(True):
-        a = math.ceil(a/2)
-        b = math.ceil(b / 2)
-        cnt+=1
-        if(a == b):
-            return cnt
+    
+    while True:
+        cnt += 1
+        a = newNum(a)
+        b = newNum(b)
+        if a == b:
+            break
+        
+    return cnt
+
+#프로그래머스 문제 예시 테스트 케이스
+print(solution(8, 4, 7))
--- DSKO/21to30/twoSeven.java
+++ DSKO/21to30/twoSeven.java
@@ -1,86 +1,35 @@
-class Node {
-    int val;
-    Node left, right;
+package DSKO.21to30;
 
-    Node(int key) {
-        val = key;
-        left = right = null;
-    }
-}
-
-class BST {
-    Node root;
-
-    BST() {
-        root = null;
-    }
-
-    void insert(int key) {
-        root = insertRec(root, key);
-    }
-
-    Node insertRec(Node root, int key) {
-        if (root == null) {
-            root = new Node(key);
-            return root;
-        }
-
-        if (key < root.val) {
-            root.left = insertRec(root.left, key);
-        } else if (key > root.val) {
-            root.right = insertRec(root.right, key);
-        }
-
-        return root;
-    }
-
-    boolean search(int key) {
-        return searchRec(root, key);
-    }
-
-    boolean searchRec(Node root, int key) {
-        if (root == null) {
-            return false;
-        }
-
-        if (root.val == key) {
-            return true;
-        }
-
-        if (key < root.val) {
-            return searchRec(root.left, key);
-        }
-
-        return searchRec(root.right, key);
-    }
-}
-
-public class twoSeven {
+class twoSeven {
+    // 프로그래머스 문제의 예시.
     public static void main(String[] args) {
-        BST bst = new BST();
+        Solution sol = new Solution();
+        int n = 8;
+        int a = 4;
+        int b = 7;
+        System.out.println(sol.solution(n, a, b));
+    }
 
-        int[] lst = { 1, 3, 5, 7, 9 };
-        int[] searchList = { 2, 4, 6, 8, 10 };
-        for (int i : lst) {
-            bst.insert(i);
+    public int solution(int n, int a, int b) {
+        int answer = 0; // 정답을 저장할 변수 answer
+
+        while (true) {
+            a = newNum(a);
+            b = newNum(b);
+            answer++;
+            if (a == b) {
+                break;
+            }
         }
 
-        System.out.print("Test case 1: ");
-        for (int i : searchList) {
-            System.out.print(bst.search(i) + " ");
-        }
-        System.out.println();
+        return answer;
+    }
 
-        int[] lst2 = { 5, 3, 8, 4, 2, 1, 7, 10 };
-        int[] searchList2 = { 1, 2, 5, 6 };
-        BST bst2 = new BST();
-        for (int i : lst2) {
-            bst2.insert(i);
-        }
-
-        System.out.print("Test case 2: ");
-        for (int i : searchList2) {
-            System.out.print(bst2.search(i) + " ");
+    private int newNum(int x) {
+        if (x % 2 == 0) {
+            return x / 2;
+        } else {
+            return (x + 1) / 2;
         }
     }
 }

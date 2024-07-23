06707a629621c680765b546ee49a4a314ee8606c
Hypeho <n85943164@gmail.com>
2024-07-23 15:42:42 +0900

    7

--- HHNAM/1to10/4.py
+++ HHNAM/1to10/4.py
@@ -19,5 +19,5 @@ def solution(answers):
             highest_scores.append(i+1)
     return highest_scores
 
-print(solution([1,2,3,4,5]))
-print(solution([1,3,2,4,2]))
+# print(solution([1,2,3,4,5]))
+# print(solution([1,3,2,4,2]))

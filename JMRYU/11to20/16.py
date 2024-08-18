a2b417284974942abe0204dd0eacb0f6121fc425
JeongMinR <ryu040408@naver.com>
2024-08-18 23:52:26 +0900

    16

--- JMRYU/11to20/16.py
+++ JMRYU/11to20/16.py
@@ -1,1 +1,20 @@
-  
+from collections import deque
+
+def solution(progresses, speeds):
+    answer = []
+    days = deque()
+
+    for i in range(len(progresses)):
+        remain = 100 - progresses[i]
+        day = (remain + speeds[i] - 1) // speeds[i]
+        days.append(day)
+        
+    while days:
+        current_day = days.popleft()  
+        count = 1
+        while days and days[0] <= current_day:
+            count += 1
+            days.popleft()
+        answer.append(count)
+
+    return answer
